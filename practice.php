c1499abe4758c1a011aa86c448a7a78b90951ac5
Samuelcsc <sanda_adeola@yahoo.com>
2018-04-25 19:17:07 +0100

    Add files via upload

---
+++ practice.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>HNG Internship</title>
+</head>
+<style>
+body{
+background-color:#E31B1B;
+}
+div{
+position: absolute;
+width: 739px;
+height: 193px;
+padding: 20px 20px 20px 80px;
+margin: 150px 250px;
+}
+h3{
+font-size:64px;
+font-family: Roboto;
+}
+</style>
+<body>
+<div>
+<h3>HNG INTERNSHIP 4.O</h3>
+<?php
+date_default_timezone_set("Africa/Lagos");
+echo date("h:i:sa");
+?>
+</div>
+</body>
+</html>
